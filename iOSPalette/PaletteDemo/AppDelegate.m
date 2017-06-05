bdd2b1a1cfd6fd74e8d594be9beb0bf249bd373c
唐笛 <564531504@qq.com>
2017-06-05 12:22:58 +0800

    干掉不用注释

--- iOSPalette/PaletteDemo/AppDelegate.m
+++ iOSPalette/PaletteDemo/AppDelegate.m
@@ -7,7 +7,6 @@
 //
 
 #import "AppDelegate.h"
-#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -18,11 +17,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-//    self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
-//    ViewController *beginVC = [[ViewController alloc]init];
-//    UINavigationController *nav = [[UINavigationController alloc]initWithRootViewController:beginVC];
-//    self.window.rootViewController = nav;
-//    [self.window makeKeyWindow];
     return YES;
 }
 
